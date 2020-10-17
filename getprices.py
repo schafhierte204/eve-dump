292ba58df49997c55db4777107b5a605d0494944
schafhierte204 <hafejens@gmail.com>
2020-10-17 22:54:22 +0200

    added low price

--- getprices.py
+++ getprices.py
@@ -4,7 +4,10 @@ payload = {'datasource':'tranquility','order_type':'sell','region_id':'10000043'
 r = requests.get("https://esi.evetech.net/latest/markets/10000043/orders", params=payload)
 orders=json.loads(r.text)
 average =[]
+low = orders[0]["price"]
 for i in orders:
     average.append(i["price"])
+    if low>i["price"]:
+        low = i["price"]
 
-print(sum(average)/len(average))
+print(" Durchsnitt="+str(sum(average)/len(average))+" Tiefstpreis="+str(low))
