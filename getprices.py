8ffac8cd2936019e9330a59a0499d193818788b5
schafhierte204 <hafejens@gmail.com>
2020-10-18 14:31:01 +0200

    finish get_prices.py

--- getprices.py
+++ getprices.py
@@ -1,13 +1,26 @@
 import json
 import requests
-payload = {'datasource':'tranquility','order_type':'sell','region_id':'10000043','type_id':'52306'}
-r = requests.get("https://esi.evetech.net/latest/markets/10000043/orders", params=payload)
-orders=json.loads(r.text)
-average =[]
-low = orders[0]["price"]
-for i in orders:
-    average.append(i["price"])
-    if low>i["price"]:
-        low = i["price"]
+from operator import itemgetter
+def get_value(id):
+    payload = {'datasource':'tranquility','order_type':'buy','region_id':'10000043','type_id':''}
+    payload['type_id']=id
+    r = requests.get("https://esi.evetech.net/latest/markets/10000043/orders", params=payload)
+    orders=json.loads(r.text)
+    orders = sorted(orders, key=itemgetter('price'), reverse=True)
+    i= 0
+    l =len(orders)
+    volume=100000000
+    while volume>0:
+        if i==l:
+            break
 
-print(" Durchsnitt="+str(sum(average)/len(average))+" Tiefstpreis="+str(low))
+        volume= volume-orders[i]['volume_remain']
+        i=i+1
+
+    average=[]
+    for j in range(0,i):
+        average.append(orders[j]['price'])
+
+    return(sum(average)/len(average))
+
+print("Tritanium="+str(get_value(34))+" Pyereite="+str(get_value(35))+" Mexallon="+str(get_value(36))+" Isogen="+str(get_value(37))+" Noxium="+str(get_value(38))+" Zydrine="+str(get_value(39))+" Megacyte="+str(get_value(40))+" Morthite="+str(get_value(11399)))
