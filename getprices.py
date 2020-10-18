9fa9bdd9edf7d5453142df8f52cae36b0d2e6cff
schafhierte204 <hafejens@gmail.com>
2020-10-18 16:40:46 +0200

    correct spelling mistake

--- getprices.py
+++ getprices.py
@@ -23,4 +23,4 @@ def get_value(id):
 
     return(sum(average)/len(average))
 
-print("Tritanium="+"{:.2f}".format(get_value(34))+" Pyereite="+"{:.2f}".format(get_value(35))+" Mexallon="+"{:.2f}".format(get_value(36))+" Isogen="+"{:.2f}".format(get_value(37))+" Noxium="+"{:.2f}".format(get_value(38))+" Zydrine="+"{:.2f}".format(get_value(39))+" Megacyte="+"{:.2f}".format(get_value(40))+" Morthite="+"{:.2f}".format(get_value(11399)))
+print("Tritanium="+"{:.2f}".format(get_value(34))+" Pyereite="+"{:.2f}".format(get_value(35))+" Mexallon="+"{:.2f}".format(get_value(36))+" Isogen="+"{:.2f}".format(get_value(37))+" Noxium="+"{:.2f}".format(get_value(38))+" Zydrine="+"{:.2f}".format(get_value(39))+" Megacyte="+"{:.2f}".format(get_value(40))+" Morphite="+"{:.2f}".format(get_value(11399)))
